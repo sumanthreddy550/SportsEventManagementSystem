3f967cf1b8f279e2c7cb757e7da5f17df3e5d734
sumanthreddy550 <57502817+sumanthreddy550@users.noreply.github.com>
2019-11-08 01:38:22 +0530

    Add files via upload

---
+++ imagedetection.py
@@ -0,0 +1,20 @@
+ # -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 22:24:20 2018
+
+@author: suman_000
+"""
+
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "flood.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+
+for eachObject in detections:
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
